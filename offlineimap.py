61fc262a129b1585132e34d7c0746e5692d3d563
Pig Monkey <pm@pig-monkey.com>
2015-12-13 12:04:25 -0800

    move keys

--- offlineimap.py
+++ offlineimap.py
@@ -2,7 +2,7 @@ import subprocess
 from os.path import expanduser
 
 def imappasswd(account):
-    path = expanduser("~/.imap-%s.gpg" % account)
+    path = expanduser("~/.keys/imap-%s.gpg" % account)
     args = ["gpg", "--use-agent", "--quiet", "--batch", "-d", path]
     try:
         return subprocess.check_output(args).strip()
