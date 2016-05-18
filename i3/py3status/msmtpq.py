5db789fff9c45b5a3103c58c3ef27fd63e29f9f4
Pig Monkey <pm@pig-monkey.com>
2016-05-17 18:06:21 -0700

    i3: use py3status and monitor msmtpq messages

---
+++ i3/py3status/msmtpq.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Display the number of messages in the msmtp queue.
+
+Configuration parameters:
+    queue: The location of the msmtp queue (default ~/.msmtp.queue)
+    hide_empty: Should a message be displayed when the queue is empty (default False)
+"""
+import os
+import time
+
+
+class Py3status:
+
+    queue = "~/.msmtp.queue"
+    hide_empty = True
+
+    def queue_count(self, i3s_output_list, i3s_config):
+        directory = os.path.expanduser(self.queue)
+        num = len([name for name in os.listdir(directory)
+                                 if os.path.isfile(os.path.join(directory, name))
+                                 and os.path.splitext(name)[-1] == '.mail'])
+
+        full_text = 'msmtpq: %s' % (num)
+        color = i3s_config['color_bad']
+
+        if num == 0:
+            color = i3s_config['color_good']
+            if self.hide_empty:
+                full_text = ''
+
+        return {
+            'full_text': full_text,
+            'cached_until': time.time() + i3s_config['interval'],
+            'color': color,
+        }
