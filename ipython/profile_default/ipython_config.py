497c2478a99acd43e4f3a45e92327bb1a9a236e9
Pig Monkey <pm@pig-monkey.com>
2018-08-13 13:45:16 -0700

    ipython: use vim keys

---
+++ ipython/profile_default/ipython_config.py
@@ -0,0 +1,11 @@
+## Set to confirm when you try to exit IPython with an EOF (Control-D in Unix,
+#  Control-Z/Enter in Windows). By typing 'exit' or 'quit', you can force a
+#  direct exit without any confirmation.
+c.TerminalInteractiveShell.confirm_exit = False
+
+## Shortcut style to use at the prompt. 'vi' or 'emacs'.
+c.TerminalInteractiveShell.editing_mode = 'vi'
+
+## Enable vi (v) or Emacs (C-X C-E) shortcuts to open an external editor. This is
+#  in addition to the F2 binding, which is always enabled.
+c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
