602b016014e7675a1b8cb1e42df516c4b01fde37
Pig Monkey <pm@pig-monkey.com>
2019-01-04 21:04:49 -0800

    mutt: use XDG_RUNTIME_DIR for printing PDFs

--- config/mutt/print.sh
+++ config/mutt/print.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env sh
 # http://terminalmage.net/2011/10/12/printing-to-pdf-in-mutt/
 INPUT="$1"
-PDIR="$HOME/tmp/mutt_print"
-OPEN_PDF=zathura
 
-# check to make sure that enscript and ps2pdf are both installed
-if ! command -v enscript >/dev/null || ! command -v ps2pdf >/dev/null; then
-    echo "ERROR: both enscript and ps2pdf must be installed" 1>&2
-    exit 1
-fi
-
-# create temp dir if it does not exist
-if [ ! -d "$PDIR" ]; then
-    mkdir -p "$PDIR" 2>/dev/null
-    if [ $? -ne 0 ]; then
-        echo "Unable to make directory '$PDIR'" 1>&2
-        exit 2
+build_dir() {
+    # Create the output directory.
+    if [ -n "$XDG_RUNTIME_DIR" ]; then
+        dir=$XDG_RUNTIME_DIR/mutt
+    else
+        dir=/tmp/$USER/mutt
     fi
-fi
+    mkdir -p "$dir"
+}
 
-tmpfile="`mktemp $PDIR/mutt_XXXXXXXX.pdf`"
+check() {
+    # Check that dependencies are installed.
+    if ! command -v enscript >/dev/null || ! command -v ps2pdf >/dev/null; then
+        echo "ERROR: both enscript and ps2pdf must be installed" 1>&2
+        exit 1
+    fi
+}
+
+check
+build_dir
+
+tmpfile="`mktemp -p "$dir" mutt_XXXXXXXX.pdf`"
 enscript --font=Courier8 $INPUT -G2r -p - 2>/dev/null | ps2pdf - $tmpfile
-$OPEN_PDF $tmpfile >/dev/null 2>&1 &
+xdg-open $tmpfile >/dev/null 2>&1 &
 sleep 1
 rm $tmpfile
