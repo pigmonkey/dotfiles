239b96efdac344651ae58b7563f5d75b0b0b4424
Pig Monkey <pm@pig-monkey.com>
2019-11-14 14:13:53 -0800

    ipython: I don't know what jedi is, but I don't want it

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -9,3 +9,6 @@ c.TerminalInteractiveShell.editing_mode = 'vi'
 ## Enable vi (v) or Emacs (C-X C-E) shortcuts to open an external editor. This is
 #  in addition to the F2 binding, which is always enabled.
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
+
+# I don't know what Jedi is supposed to do, but it is ridiculously slow.
+c.IPCompleter.use_jedi = False
